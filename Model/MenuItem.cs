0e91368ccb76737a54f3e75c9d84f68271c4cfb9
hendrikmolgaard <Moelgaard01@hotmail.com>
2024-11-12 01:11:01 +0100

    Henter menu dynamisk fra Azure Table Storage

---
+++ Model/MenuItem.cs
@@ -0,0 +1,13 @@
+namespace IBAS_kantine.Model
+
+{
+
+
+    public class MenuItem
+    {
+        public string? Uge { get; set; }
+        public string? Dag { get; set; }
+        public string? KoldRet { get; set; }
+        public string? VarmRet { get; set; }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,36 @@
-using Microsoft.AspNetCore.Mvc;
+using Azure.Data.Tables;
+using IBAS_kantine.Model;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using Azure;
 
-namespace IBAS_kantine.Pages;
-
-public class IndexModel : PageModel
+namespace IBAS_kantine.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        public List<MenuItem> MenuItems { get; set; }
 
-    public void OnGet()
-    {
+        public void OnGet()
+        {
+            var tablename = "UgensMenu";
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=ibasbikeproduction51;AccountKey=3mtytlejwLNfvU2DHU+mO1cjlB4MspS3RPi5TonVGYwi0HBXnL9CUzn7jfbdZVA+3iHbKcVev5dh+ASt5ON5TA==;EndpointSuffix=core.windows.net";
 
+            TableClient tableClient = new TableClient(connectionString, tablename);
+            Pageable<TableEntity> items = tableClient.Query<TableEntity>();
+
+            MenuItems = new List<MenuItem>();
+
+            foreach (TableEntity item in items)
+            {
+                var dto = new MenuItem()
+                {
+                    Dag = item.RowKey,
+                    Uge = item.PartitionKey,
+                    VarmRet = item.GetString("VarmRet"),
+                    KoldRet = item.GetString("KoldRet")
+                };
+                MenuItems.Add(dto);
+            }
+        }
     }
 }
